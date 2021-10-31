85ebe9be4e329c24ff1c29d97684c525009f4613
GameGenesis <gamegenesisdev@gmail.com>
2021-10-31 00:51:48 -0400

    Add Vector2 Operators & Normalization

--- NumDotNet/Vector2.cs
+++ NumDotNet/Vector2.cs
@@ -32,6 +32,8 @@ namespace NumDotNet
         public float sqrMagnitude => x * x + y * y;
         /// <summary> Returns the length of this vector (Read Only). </summary>
         public float magnitude => (float)Math.Sqrt(sqrMagnitude);
+        /// <summary> Returns this vector with a magnitude of 1 (Read Only). </summary>
+        public Vector2 normalized => this / magnitude;
 
         /// <summary> Access the x or y component using [0] or [1] respectively. </summary>
         public float this[int index]
@@ -43,6 +45,19 @@ namespace NumDotNet
                 else throw new IndexOutOfRangeException();
             }
         }
+
+        /// <summary> Adds two Vectors. </summary>
+        public static Vector2 operator +(Vector2 a, Vector2 b) => new Vector2(a.x + b.x, a.y + b.y);
+        /// <summary> Subtracts one vector from another. </summary>
+        public static Vector2 operator -(Vector2 a, Vector2 b) => new Vector2(a.x - b.x, a.y - b.y);
+        /// <summary> Multiplies a vector by a number. </summary>
+        public static Vector2 operator *(Vector2 a, float d) => new Vector2(a.x * d, a.y * d);
+        /// <summary> Divides a vector by a number. </summary>
+        public static Vector2 operator /(Vector2 a, float d) => d != 0 ? new Vector2(a.x / d, a.y / d) : throw new DivideByZeroException();
+        /// <summary> Returns true if two vectors are approximately equal. </summary>
+        public static bool operator ==(Vector2 a, Vector2 b) => Math.Abs(a.x - b.x) <= 0.00001f && Math.Abs(a.y - b.y) <= 0.00001f;
+        /// <summary> Returns true if two vectors are not equal. </summary>
+        public static bool operator !=(Vector2 a, Vector2 b) => Math.Abs(a.x - b.x) > 0.00001f || Math.Abs(a.y - b.y) > 0.00001f;
 
         /// <summary> Constructs a new vector with given x, y components. </summary>
         public Vector2(float x, float y)
@@ -59,10 +74,10 @@ namespace NumDotNet
         }
 
         /// <summary> Makes this vector have a magnitude of 1. </summary>
-        public void Normalize(float newX, float newY)
+        public void Normalize()
         {
-            this.x = newX;
-            this.y = newY;
+            x /= magnitude;
+            y /= magnitude;
         }
 
         /// <summary> Returns true if the given vector is exactly equal to this vector. </summary>
