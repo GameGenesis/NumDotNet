0981943e7329b799a911055cac2fa649b39bcce7
GameGenesis <gamegenesisdev@gmail.com>
2021-11-06 01:26:56 -0400

    Add Vector3 static properties

--- NumDotNet/src/Vector3.cs
+++ NumDotNet/src/Vector3.cs
@@ -21,5 +21,26 @@ namespace NumDotNet
             this.y = y;
             this.z = z;
         }
+
+        /// <summary> Shorthand for writing Vector3(0, 0, -1). </summary>
+        public static Vector3 back => new Vector3(0, 0, -1);
+        /// <summary> Shorthand for writing Vector3(0, 0, 1). </summary>
+        public static Vector3 forward => new Vector3(0, 0, 1);
+        /// <summary> Shorthand for writing Vector3(0, -1, 0). </summary>
+        public static Vector3 down => new Vector3(0, -1, 0);
+        /// <summary> Shorthand for writing Vector3(0, 1, 0). </summary>
+        public static Vector3 up => new Vector3(0, 1, 0);
+        /// <summary> Shorthand for writing Vector3(-1, 0, 0). </summary>
+        public static Vector3 left => new Vector3(-1, 0, 0);
+        /// <summary> Shorthand for writing Vector3(1, 0, 0). </summary>
+        public static Vector3 right => new Vector3(1, 0, 0);
+        /// <summary> Shorthand for writing Vector3(0, 0, 0). </summary>
+        public static Vector3 zero => new Vector3(0, 0, 0);
+        /// <summary> Shorthand for writing Vector3(1, 1, 1). </summary>
+        public static Vector3 one => new Vector3(1, 1, 1);
+        /// <summary> Shorthand for writing Vector3(float.NegativeInfinity, float.NegativeInfinity, float.NegativeInfinity). </summary>
+        public static Vector3 negativeInfinity => new Vector3(float.NegativeInfinity, float.PositiveInfinity, float.NegativeInfinity);
+        /// <summary> Shorthand for writing Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity). </summary>
+        public static Vector3 positiveInfinity => new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity);
     }
 }
