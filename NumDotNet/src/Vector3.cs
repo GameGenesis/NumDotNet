c8ccf6a0750bca409a81af041dbb488c31b92b7f
GameGenesis <gamegenesisdev@gmail.com>
2021-11-06 13:58:07 -0400

    Update Vector3 comments to improve code readability

--- NumDotNet/src/Vector3.cs
+++ NumDotNet/src/Vector3.cs
@@ -6,17 +6,29 @@ namespace NumDotNet
 {
     public struct Vector3
     {
-        /// <summary> X component of the vector. </summary>
+        /// <summary>
+        /// X component of the vector.
+        /// </summary>
         public float x { get; set; }
-        /// <summary> Y component of the vector. </summary>
+
+        /// <summary>
+        /// Y component of the vector.
+        /// </summary>
         public float y { get; set; }
-        /// <summary> Z component of the vector. </summary>
+
+        /// <summary>
+        /// Z component of the vector.
+        /// </summary>
         public float z { get; set; }
 
-        /// <summary> Constructs a new vector with given x, y points. </summary>
+        /// <summary>
+        /// Constructs a new vector with given x, y points.
+        /// </summary>
         public Vector3(float x, float y) : this(x, y, 0) { }
 
-        /// <summary> Constructs a new vector with given x, y, z components. </summary>
+        /// <summary>
+        /// Constructs a new vector with given x, y, z components.
+        /// </summary>
         public Vector3(float x, float y, float z)
         {
             this.x = x;
@@ -24,7 +36,9 @@ namespace NumDotNet
             this.z = z;
         }
 
-        /// <summary> Constructs a new vector with given Vector2 x, y values. </summary>
+        /// <summary>
+        /// Constructs a new vector with given Vector2 x, y values.
+        /// </summary>
         public Vector3(Vector2 v)
         {
             this.x = v.x;
@@ -32,7 +46,9 @@ namespace NumDotNet
             this.z = 0;
         }
 
-        /// <summary> Constructs a new vector with given Vector3 x, y, z values. </summary>
+        /// <summary>
+        /// Constructs a new vector with given Vector3 x, y, z values.
+        /// </summary>
         public Vector3(Vector3 v)
         {
             this.x = v.x;
@@ -40,7 +56,9 @@ namespace NumDotNet
             this.z = v.z;
         }
 
-        /// <summary> Constructs a new vector with given array values (superfluous values are ignored, missing values are zero-filled). </summary>
+        /// <summary>
+        /// Constructs a new vector with given array values (superfluous values are ignored, missing values are zero-filled).
+        /// </summary>
         public Vector3(float[] v, int startIndex = 0)
         {
             this.x = v.Length < 1 + startIndex ? 0f : v[startIndex];
@@ -48,28 +66,60 @@ namespace NumDotNet
             this.z = v.Length < 3 + startIndex ? 0f : v[startIndex + 2];
         }
 
-        /// <summary> Constructs a new vector with given IEnumerable values (superfluous values are ignored, missing values are zero-filled). </summary>
+        /// <summary>
+        /// Constructs a new vector with given IEnumerable values (superfluous values are ignored, missing values are zero-filled).
+        /// </summary>
         public Vector3(IEnumerable<float> v, int startIndex = 0) : this(v.ToArray(), startIndex) { }
 
-        /// <summary> Shorthand for writing Vector3(0, 0, -1). </summary>
+        /// <summary>
+        /// Shorthand for writing Vector3(0, 0, -1).
+        /// </summary>
         public static Vector3 back => new Vector3(0, 0, -1);
-        /// <summary> Shorthand for writing Vector3(0, 0, 1). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(0, 0, 1).
+        /// </summary>
         public static Vector3 forward => new Vector3(0, 0, 1);
-        /// <summary> Shorthand for writing Vector3(0, -1, 0). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(0, -1, 0).
+        /// </summary>
         public static Vector3 down => new Vector3(0, -1, 0);
-        /// <summary> Shorthand for writing Vector3(0, 1, 0). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(0, 1, 0).
+        /// </summary>
         public static Vector3 up => new Vector3(0, 1, 0);
-        /// <summary> Shorthand for writing Vector3(-1, 0, 0). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(-1, 0, 0).
+        /// </summary>
         public static Vector3 left => new Vector3(-1, 0, 0);
-        /// <summary> Shorthand for writing Vector3(1, 0, 0). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(1, 0, 0).
+        /// </summary>
         public static Vector3 right => new Vector3(1, 0, 0);
-        /// <summary> Shorthand for writing Vector3(0, 0, 0). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(0, 0, 0).
+        /// </summary>
         public static Vector3 zero => new Vector3(0, 0, 0);
-        /// <summary> Shorthand for writing Vector3(1, 1, 1). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(1, 1, 1).
+        /// </summary>
         public static Vector3 one => new Vector3(1, 1, 1);
-        /// <summary> Shorthand for writing Vector3(float.NegativeInfinity, float.NegativeInfinity, float.NegativeInfinity). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(float.NegativeInfinity, float.NegativeInfinity, float.NegativeInfinity).
+        /// </summary>
         public static Vector3 negativeInfinity => new Vector3(float.NegativeInfinity, float.PositiveInfinity, float.NegativeInfinity);
-        /// <summary> Shorthand for writing Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity). </summary>
+
+        /// <summary>
+        /// Shorthand for writing Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity).
+        /// </summary>
         public static Vector3 positiveInfinity => new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity);
+
     }
 }
